ef32f31c6c4278c86c311d0610deb4963a95c193
Wilson Tovar <twilson@uninorte.edu.co>
2017-07-23 22:24:12 -0500

    API :D

--- Julia/background.jl
+++ Julia/background.jl
@@ -1,13 +1,14 @@
 using PyCall, FileIO
 @pyimport SimpleCV
 
-module BackgroundModule
+module Background
+  """ Módulo con todas las funciones de procesamiento de imágenes """
+
   function backg(path::String, save_path::String)
     """ Eliminar el fondo blanco de una imagen
-        Keyword arguments:
-        path -- la carpeta que contiene las imagenes
-        save_path -- la carpeta donde se guardaran las imagenes modificadas
-        extension -- extension de las imagenes (default: *.jpg) (deprecated)
+
+        path : la carpeta que contiene las imágenes
+        save_path : la carpeta donde se guardarán las imágenes modificadas
     """
     init = Float64(time())
     dir = Array{String,1}(readdir(path))
@@ -19,7 +20,8 @@ module BackgroundModule
       result[:save](save_path*file)
     end
 
-    end_time = Float64(time())  # Tiempo final
+
+    end_time = Float64(time())
     info(string(length(dir)," imagenes procesadas. Tiempo: ",end_time - init," segs."))
     return string(length(dir)," imagenes procesadas. Tiempo: ",end_time - init," segs.")
   end
@@ -27,9 +29,14 @@ module BackgroundModule
   export backg
 end
 
+module API
+  """ Implementación de una pequeña API """
+  # TODO Implementar
+end
+
 if length(ARGS) == 2
     using BackgroundModule
-    BackgroundModule.backg(ARGS[1], ARGS[2])
+    backg(ARGS[1], ARGS[2])
 else
-    error("Debe escribir todos los argumentos.")
+    warn("Debe escribir todos los argumentos.")
 end
