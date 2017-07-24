51f26e1a3a3057a4acf7467083e775f3e5489367
Wilson Tovar <twilson@uninorte.edu.co>
2017-07-23 21:49:29 -0500

    Bye, bye, API :(

--- Julia/api.jl
+++
@@ -1,4 +0,0 @@
-using JuliaWebAPI
-using Compat
-
-process([], "tcp://127.0.0.1:4040"; bind= true)
--- Julia/background.jl
+++ Julia/background.jl
@@ -1,31 +1,35 @@
 using PyCall, FileIO
 @pyimport SimpleCV
 
-function backg(path::String, save_path::String)
-  """ Eliminar el fondo blanco de una imagen
-      Keyword arguments:
-      path -- la carpeta que contiene las imagenes
-      save_path -- la carpeta donde se guardaran las imagenes modificadas
-      extension -- extension de las imagenes (default: *.jpg) (deprecated)
-  """
-  init = Float64(time())
-  dir = Array{String,1}(readdir(path))
+module BackgroundModule
+  function backg(path::String, save_path::String)
+    """ Eliminar el fondo blanco de una imagen
+        Keyword arguments:
+        path -- la carpeta que contiene las imagenes
+        save_path -- la carpeta donde se guardaran las imagenes modificadas
+        extension -- extension de las imagenes (default: *.jpg) (deprecated)
+    """
+    init = Float64(time())
+    dir = Array{String,1}(readdir(path))
 
-  for file in dir
-    img = SimpleCV.Image(path*file)
-    mask = img[:hueDistance]()[:binarize]()
-    result = img - mask[:invert]()
-    result[:save](save_path*file)
+    for file in dir
+      img = SimpleCV.Image(path*file)
+      mask = img[:hueDistance]()[:binarize]()
+      result = img - mask[:invert]()
+      result[:save](save_path*file)
+    end
+
+    end_time = Float64(time())  # Tiempo final
+    info(string(length(dir)," imagenes procesadas. Tiempo: ",end_time - init," segs."))
+    return string(length(dir)," imagenes procesadas. Tiempo: ",end_time - init," segs.")
   end
 
-  end_time = Float64(time())  # Tiempo final
-  info(string(length(dir)," imagenes procesadas. Tiempo: ",end_time - init," segs."))
-  return string(length(dir)," imagenes procesadas. Tiempo: ",end_time - init," segs.")
+  export backg
 end
 
-# __name__ == "__main__"
 if length(ARGS) == 2
-  backg(ARGS[1], ARGS[2])
+    using BackgroundModule
+    BackgroundModule.backg(ARGS[1], ARGS[2])
 else
-  error("Debe especificar todos los argumentos necesarios.")
+    error("Debe escribir todos los argumentos.")
 end
